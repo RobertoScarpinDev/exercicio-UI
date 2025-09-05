5d303e341ef411f88bc9952285b9ca8c242f8282
Roberto <betoscarpin@gmail.com>
2025-09-05 08:31:09 -0300

    teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('beto.teste1910@teste.com.br')
+        cy.get('#password').type('12345')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, beto.teste1910')
+        
+    });
+    
+});
